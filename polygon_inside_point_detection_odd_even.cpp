0e3b3b94b843f1e84fd6ce645caea215c4d8e91f
Rustam Ali Ahmed <ahmed.rustam77@gmail.com>
2023-09-13 10:44:46 +0530

    error fixed

--- polygon_inside_point_detection_odd_even.cpp
+++ polygon_inside_point_detection_odd_even.cpp
@@ -2,27 +2,32 @@
 #include <graphics.h>
 
 // Define the polygon vertices as an array of x and y coordinates.
-int polyVertices[] = {100, 100, 200, 100, 200, 200, 100, 200};
+int polyVertices[] = {100, 100, 200, 100, 200, 200, 100, 200, 100, 100};
 int numVertices = 4; // Number of vertices in the polygon
 
 // Function to draw the polygon
 void drawPolygon()
 {
-    drawpoly(numVertices, polyVertices);
+    drawpoly(numVertices + 1, polyVertices);
 }
 
-// Function to check if a point is inside the polygon using the odd-even rule 
+// Function to check if a point is inside the polygon using the odd-even rule
 int isInsidePolygon(int x, int y)
 {
     int oddNodes = 0;
     int j = numVertices - 1;
+    putpixel(x, y, RED);
+    printf("\nPoint Position: %d\t%d\n", x, y);
 
     for (int i = 0; i < numVertices; i++)
     {
+        j = i + 1;
         int xi = polyVertices[i * 2];
         int yi = polyVertices[i * 2 + 1];
         int xj = polyVertices[j * 2];
         int yj = polyVertices[j * 2 + 1];
+
+        printf("%d\t%d\t%d\t%d\n", xi, yi, xj, yj);
 
         if ((yi < y && yj >= y) || (yj < y && yi >= y))
         {
@@ -44,9 +49,23 @@ int main()
     initgraph(&gd, &gm, NULL);
 
     drawPolygon();
-    int testPointX = 350;
+    int testPointX = 120;
     int testPointY = 150;
 
+    if (isInsidePolygon(testPointX, testPointY))
+    {
+        // outtextxy(10, 10, "Point is inside the polygon.");
+        printf("Point is inside the polygon.\n");
+    }
+    else
+    {
+        // outtextxy(10, 10, "Point is outside the polygon.");
+        printf("Point is outside the polygon.\n");
+    }
+
+    // test with 2nd point
+    testPointX = 320;
+    testPointY = 150;
     if (isInsidePolygon(testPointX, testPointY))
     {
         // outtextxy(10, 10, "Point is inside the polygon.");
---
+++ readme.txt
@@ -0,0 +1,10 @@
+C:\MinGW\bin\g++.exe -o a.exe line_dda.cpp -lbgi -lgdi32 -lcomdlg32 -luuid -loleaut32 -lole32#
+
+
+echo "# CG-with-CPP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rustamtu/CG-with-CPP.git
+git push -u origin main
